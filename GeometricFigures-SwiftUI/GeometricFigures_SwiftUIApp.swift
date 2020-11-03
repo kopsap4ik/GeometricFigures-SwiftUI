cbeb2aa8a97dd185ad99db23fb14b91cb9cc1c32
Vasily Petuhov <vspetukhov@icloud.com>
2020-11-03 14:01:25 +0300

    Initial Commit

---
+++ GeometricFigures-SwiftUI/GeometricFigures_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFigures_SwiftUIApp.swift
+//  GeometricFigures-SwiftUI
+//
+//  Created by Василий Петухов on 03.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFigures_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
